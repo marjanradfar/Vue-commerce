b49d20cf0850bd9561cd4740f3282695bb0ea831
Marjan <info.marjan.radfar@gmail.com>
2024-04-12 12:49:20 +0330

    v-if in component card

--- src/components/common/Card.vue
+++ src/components/common/Card.vue
@@ -3,19 +3,20 @@
     <div class="wrapperCard__imgContainer">
       <img class="wrapperCard__imgContainer--imgCard" :src="data.imgProduct" />
       <div class="discountNew">
-        <Button :text="data.btnNew" className="btnNew"/>
-        <Button :text="data.btnDiscount" className="btnDiscount"/>
+        <Button :text="data.btnNew" className="btnNew" v-if="!!data.btnNew"/>
+        <Button :text="data.btnDiscount" className="btnDiscount" v-if="!!data.btnDiscount"/>
       </div>
-      <Button :text="data.btnAddToCard" className="btnAddToCard btnBlack"/>
+      <Button :text="data.btnAddToCard" className="btnAddToCard btnBlack" v-if="!!data.btnAddToCard"/>
     </div>
     <div class="wrapperCard__content">
       <div class="wrapperCard__content--rating">
-        <img :src="rate.imgRating" v-for="(rate , id) in data.rate " :key="id">
+        <img :src="rate.imgRating" v-for="(rate , id) in data.rate " :key="id" v-if="!!data.rate">
       </div>
-      <span class="wrapperCard__content--title">{{data.title}}</span>
+      <p class="wrapperCard__content--title">{{data.title}}</p>
       <div class="wrapperCard__content--price">
-        <span class="priceMain smallLightText">{{data.priceMain}}</span>
-        <span class="priceOff smallLightText">{{data.priceOff}}</span>
+        <span class="priceMain smallLightText" v-if="!!data.priceMain">{{data.priceMain}}</span>
+        <span class="priceOff smallLightText" v-if="!!data.priceOff">{{data.priceOff}}</span>
+        <span class="date" v-if="!!data.date">{{data.date}}</span>
       </div>
     </div>
   </div>
