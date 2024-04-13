afcff545487427bf3e14acb5023548040734943f
Marjan <info.marjan.radfar@gmail.com>
2024-04-13 13:14:37 +0330

    restyle components opinionCard

--- src/components/OpinionCard.vue
+++ src/components/OpinionCard.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="opinionContainer">
-    <div class="pinionContainer__img">
-      <img :src="data.img">
-    </div>
-    <div class="opinionContainer__opinions">
-      <div class="opinionContainer__opinions--head">
+    <div class="opinionContainer__head">
+      <img :src="data.img" class="opinionContainer__head--img">
+      <div class="opinionContainer__head--title">
         <strong class="peopleName">{{ data.peopleName }}</strong>
         <div class="rate">123456</div>
       </div>
+
+    </div>
+    <div class="opinionContainer__opinions">
       <p class="opinionContainer__opinions--text">
-        {{data.text}}
+        {{ data.text }}
       </p>
       <div class="opinionContainer__opinions--action">
-        <span class="releaseTime" v-for="(item , index) in data.action">{{item.title}}</span>
+        <span class="releaseTime" v-for="(item , index) in data.action">{{ item.title }}</span>
       </div>
     </div>
+    <hr/>
   </div>
 
 </template>
@@ -24,27 +26,46 @@ const props = defineProps(["data"]);
 </script>
 
 <style lang="scss">
+@import "../assets/style/base/responsive";
+@import "../assets/style/base/variable";
+
 .opinionContainer {
   display: flex;
-  gap: 40px;
-  //margin-bottom: 24px;
-  &__opinions {
+  flex-direction: column;
 
-    border-bottom: 1px solid red;
-    &--head{
-      display: flex;
-      align-content: center;
-      flex-direction: column;
-      gap: 16px;
+  &__head {
+    display: flex;
+    gap: 40px;
+    align-items: center;
+    @include break(480){
+      gap: 1rem;
+      margin-bottom: 1rem;
     }
-    &--text{
+    &--title {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+    }
+  }
+
+  &__opinions {
+    margin-right: 109px;
+    border-bottom: 1px solid $FourthTextColor;
+    @include break(480){
+      margin-right: 0;
+    }
+    &--text {
       margin-bottom: 24px;
     }
+
     &--action {
       display: flex;
       gap: 1.5rem;
       margin-bottom: 24px;
     }
+  }
+  hr{
+    background-color: $FourthTextColor;
   }
 }
 
